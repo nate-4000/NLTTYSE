01cf805076901cbf7731b0fd64392426c12d9014
nate-4000 <nathanieltebo8@gmail.com>
2023-04-07 13:18:03 -0700

    ok really python 3.11 support now

--- updatelib.bat
+++ updatelib.bat
@@ -1,6 +1,6 @@
-REM put the cwd in python's funni place for funni reasons
+REM put the cwd in python's lib directory so that we can import from other scripts
 CD %LocalAppData%\Programs\Python\Python311\Lib
 REM copy the file there
 REM wtf is %~dp0
 REM "." dont work, but why?
-COPY %~dp0\screen.py %LocalAppData%\Programs\Python\Python310\Lib
+COPY %~dp0\screen.py %LocalAppData%\Programs\Python\Python311\Lib
